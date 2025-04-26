89a6b1f3a1b61d24de9c7c316a5e25d56e73628f
Henrique Barucco <henrique_barucco@hotmail.com>
2025-04-26 18:20:23 -0300

    chore: add build src

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("application")
-    kotlin("jvm") version "2.1.10"
+    kotlin("jvm")
     kotlin("plugin.spring") version "2.1.20"
     id("org.springframework.boot") version "3.4.5"
     id("io.spring.dependency-management") version "1.1.7"
